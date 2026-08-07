b72c0851114ff1dadc7c52ea81884cce822d7b64
Sneha Sahu <snehastudy1103@gmail.com>
2026-08-08 00:30:12 +0530

    fix: import official openai package in aiController

--- src/controllers/aiController.js
+++ src/controllers/aiController.js
@@ -1,4 +1,4 @@
-const OpenAI = require('@openai/api');
+const OpenAI = require('openai');
 const prisma = require('../prismaClient');
 
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
